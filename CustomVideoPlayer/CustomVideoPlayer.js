3b8d8596081830b62979c91f0cff0e36381a4f38
Akshar Mistry <mistry.aks@gmail.com>
2019-11-30 20:50:54 +0530

    update

--- CustomVideoPlayer/CustomVideoPlayer.js
+++ CustomVideoPlayer/CustomVideoPlayer.js
@@ -1,5 +1,11 @@
 
 function toggleVideo() {
     const videoPlayer = document.getElementById('video-player');
+    const playIcon = document.getElementById("playIcon");
     videoPlayer.paused ? videoPlayer.play() : videoPlayer.pause(); 
+    videoPlayer.paused
+      ? playIcon.classList.remove("fa-pause") &&
+        playIcon.classList.add("fa-play")
+      : playIcon.classList.remove("fa-play") &&
+        playIcon.classList.add("fa-pause");
 }
