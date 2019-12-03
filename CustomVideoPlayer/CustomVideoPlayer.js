42469ae828b6610e652607d3fdd3074ec7526f5c
Akshar Mistry <mistry.aks@gmail.com>
2019-12-03 15:23:09 +0530

    Toggle Play Pause Controls

--- CustomVideoPlayer/CustomVideoPlayer.js
+++ CustomVideoPlayer/CustomVideoPlayer.js
@@ -1,11 +1,23 @@
 
+const videoPlayer = document.getElementById('video-player');
+const playIcon = document.getElementById("playIcon");
+const volumeSlider = document.getElementById("volumeSlider");
+
 function toggleVideo() {
-    const videoPlayer = document.getElementById('video-player');
-    const playIcon = document.getElementById("playIcon");
-    videoPlayer.paused ? videoPlayer.play() : videoPlayer.pause(); 
-    videoPlayer.paused
-    ? playIcon.classList.add("fa-pause") &&
-        playIcon.classList.add("fa-play")
-      : playIcon.classList.remove("fa-play") &&
-        playIcon.classList.add("fa-pause");
+    if( videoPlayer.paused ){
+      videoPlayer.play();
+      playIcon.classList.add("fa-pause");
+      playIcon.classList.remove("fa-play");
+    }
+    else{
+      videoPlayer.pause();
+      playIcon.classList.add("fa-play");
+      playIcon.classList.remove("fa-pause");
+    }
+}
+
+function toggleVolume() {
+  let volumeToSet = volumeSlider.value > 0 ? parseFloat(volumeSlider.value / 100) : 1;
+  console.log("Volume: " , volumeToSet);
+  videoPlayer.volume = volumeToSet;
 }
